8d68d70b3e1703f81ce4b8dad981882f340bac62
Loz <pdlo814@gmail.com>
2025-06-12 23:06:31 +0800

    feat: add Pinia for state management

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,24 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
-import Home from './views/Home.vue'
-import Quiz from './views/Quiz.vue'
-import Result from './views/Result.vue'
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import Home from "./views/Home.vue";
+import Quiz from "./views/Quiz.vue";
+import Result from "./views/Result.vue";
+import "./assets/main.css";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Home },
-    { path: '/quiz', component: Quiz },
-    { path: '/result', component: Result },
-  ]
-})
+    { path: "/", component: Home },
+    { path: "/quiz", component: Quiz },
+    { path: "/result", component: Result },
+  ],
+});
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(router);
+app.use(pinia);
+app.mount("#app");
